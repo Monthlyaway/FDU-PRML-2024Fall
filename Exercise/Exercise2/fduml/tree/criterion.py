160f8c3f3fe21c68d73af84de6e5165813c4dee5
Wilson Yang <wilsonyang315@gmail.com>
2024-10-07 15:14:37 +0800

    Exercise 2: Implement criterion

--- Exercise/Exercise2/fduml/tree/criterion.py
+++ Exercise/Exercise2/fduml/tree/criterion.py
@@ -37,6 +37,18 @@ def __label_stat(y, l_y, r_y):
     return all_labels, left_labels, right_labels
 
 
+def __entropy(labels):
+    total_count = sum(labels.values())
+    result = .0
+
+    for count in labels.values():
+        if count > 0:
+            p = count / total_count
+            result += -p * math.log2(p)
+
+    return result
+
+
 def __info_gain(y, l_y, r_y):
     """
     Calculate the info gain
@@ -51,7 +63,15 @@ def __info_gain(y, l_y, r_y):
     # l_y and r_y                                                             #
     ###########################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+    entropy_parent = __entropy(all_labels)
+    entropy_left = __entropy(left_labels)
+    entropy_right = __entropy(right_labels)
 
+    num_parent = sum(all_labels.values())
+    num_left = sum(left_labels.values())
+    num_right = sum(right_labels.values())
+    info_gain = entropy_parent - num_left / num_parent * \
+        entropy_left - num_right / num_parent * entropy_right
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
     return info_gain
@@ -70,6 +90,15 @@ def __info_gain_ratio(y, l_y, r_y):
     # into l_y and r_y                                                        #
     ###########################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+    num_parent = len(y.reshape(-1))
+    num_left = len(l_y.reshape(-1))
+    num_right = len(r_y.reshape(-1))
+
+    intrinsic_value = - num_left / num_parent * \
+        math.log2(num_left / num_parent) - num_right / \
+        num_parent * math.log2(num_right / num_parent)
+
+    info_gain = info_gain / intrinsic_value
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
     return info_gain
@@ -89,6 +118,30 @@ def __gini_index(y, l_y, r_y):
     # after splitting y into l_y and r_y                                      #
     ###########################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+    # Get label distributions for parent, left, and right nodes
+    all_labels, left_labels, right_labels = __label_stat(y, l_y, r_y)
+
+    # Helper function to calculate Gini for a given set of labels
+    def gini(labels):
+        total = sum(labels.values())
+        gini_value = 1.0
+        for count in labels.values():
+            p = count / total
+            gini_value -= p ** 2
+        return gini_value
+
+    # Calculate Gini for parent, left, and right nodes
+    before = gini(all_labels)
+    gini_left = gini(left_labels)
+    gini_right = gini(right_labels)
+
+    # Number of samples in parent and child nodes
+    num_parent = sum(all_labels.values())
+    num_left = sum(left_labels.values())
+    num_right = sum(right_labels.values())
+
+    after = (num_left / num_parent) * gini_left + \
+        (num_right / num_parent) * gini_right
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
     return before - after
@@ -104,6 +157,21 @@ def __error_rate(y, l_y, r_y):
     # after splitting y into l_y and r_y                                      #
     ###########################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+    def error_rate(labels):
+        total = sum(labels.values())
+        if total == 0:
+            return 0
+        majority = max(labels.values())
+        return 1 - majority / total
+
+    before = error_rate(all_labels)
+    left_rate = error_rate(left_labels)
+    right_rate = error_rate(right_labels)
+
+    num_parent = sum(all_labels.values())
+    num_left = sum(left_labels.values())
+    num_right = sum(right_labels.values())
+    after = num_left / num_parent * left_rate + num_right / num_parent * right_rate
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
     return before - after
